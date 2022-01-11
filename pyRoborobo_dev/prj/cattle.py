e6cb0a61089471c212dda62d36e0324069957501
Scott Hallauer <scott.hallauer@gmail.com>
2022-01-11 08:41:55 +0200

    Add custom properties and basic controller specialisation

--- pyRoborobo_dev/prj/cattle.py
+++ pyRoborobo_dev/prj/cattle.py
@@ -1,6 +1,6 @@
 from pyroborobo import Pyroborobo, Controller
 import numpy as np
-import random
+from configparser import ConfigParser
 
 
 def principal_value(deg):
@@ -14,6 +14,12 @@ def principal_value(deg):
 def angle_diff(x, y):
     return principal_value(x - y)
 
+def is_robot(id, props):
+  return id < props["cMaxRobotNumber"]
+
+def is_cattle(id, props):
+  return not is_robot(id, props)
+
 class CattleController(Controller):
 
     def __init__(self, world_model):
@@ -24,26 +30,30 @@ class CattleController(Controller):
         self.camera_max_range = 0
         self.repulse_radius = 0
         self.orientation_radius = 0
-        self.flag = False
-      
-        # color = [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
-        # self.set_color(*color[random.randrange(0,3)])
-
-        # print(self.get_id())
+        self.props = {}
+        config = ConfigParser()
+        with open("config/cattle.properties") as stream:
+          config.read_string("[root]\n" + stream.read())
+        self.props["gInitialNumberOfRobots"] = int(config.get("root", "gInitialNumberOfRobots"))
+        self.props["cMaxRobotNumber"] = int(config.get("root", "cMaxRobotNumber"))
+        self.props["cMaxCattleNumber"] = int(config.get("root", "cMaxCattleNumber"))
+        if is_robot(self.get_id(), self.props):
+          self.type = "robot"
+          self.set_color(*[255, 0, 0])
+        else:
+          self.type = "cattle"
+          self.set_color(*[0, 255, 0])
 
     def reset(self):
         self.repulse_radius = 0.2
         self.orientation_radius = 0.6
 
     def step(self):  # step is called at each time step
-        self.set_translation(1)  # Let's go forward
+        if is_robot(self.get_id(), self.props):
+          self.set_translation(0.25)  # Let's go forward
+        else:
+          self.set_translation(1)  # Let's go forward
         self.set_rotation(0)
-
-        if not self.flag:
-          robot_cnt = len(self.rob.controllers)
-          if self.get_id() < (robot_cnt / 4):
-            self.set_color(*[0, 255, 0])
-
         camera_dist = self.get_all_distances()
         camera_id = self.get_all_robot_ids()
         camera_angle_rad = self.get_all_sensor_angles()
@@ -59,10 +69,11 @@ class CattleController(Controller):
                         self.set_rotation(-camera_angle_rad[i] / np.pi)
                     else:
                         self.set_rotation(1)
-                elif dist < self.orientation_radius and camera_id[i] != -1:
+                elif is_cattle(self.get_id(), self.props):
+                  if dist < self.orientation_radius and camera_id[i] != -1 and is_cattle(self.get_robot_id_at(i), self.props):
                     orient_angle = self.get_robot_relative_orientation_at(i)
                     self.set_rotation(orient_angle / np.pi)
-                elif dist < self.camera_max_range and camera_id[i] != -1:
+                  elif dist < self.camera_max_range and camera_id[i] != -1:
                     self.set_rotation(camera_angle_rad[i] / np.pi)
                 break  # stop
 
