8fdb6eaceac7f1e401219e26295d58795b4ba024
Scott Hallauer <scott.hallauer@gmail.com>
2022-02-17 14:31:17 +0200

    Move agent-specific attributes into sub-controllers

--- pyRoborobo_dev/prj/herd.py
+++ pyRoborobo_dev/prj/herd.py
@@ -3,13 +3,13 @@ import numpy as np
 from configparser import ConfigParser
 
 
+
 def principal_value(deg):
   deg_mod = np.mod(deg, 360)
   if deg_mod > 180:
     return deg_mod - 360
   else:
     return deg_mod
-
 
 def angle_diff(x, y):
   return principal_value(x - y)
@@ -20,22 +20,18 @@ def is_shepherd(id, props):
 def is_cattle(id, props):
   return not is_shepherd(id, props)
 
+
+
 class AgentController(Controller):
 
   config_filename = "config/herd.properties"
 
   def __init__(self, world_model):
-    # It is *mandatory* to call the super constructor before any other operation to
-    # link the python object to its C++ counterpart
-    Controller.__init__(self, world_model)
-    self.rob = Pyroborobo.get()
-    self.camera_max_range = 0
-    self.repulse_radius = 0
-    self.orientation_radius = 0
-    self.props = {}
+    Controller.__init__(self, world_model) # mandatory call to super constructor
     config = ConfigParser()
     with open(AgentController.config_filename) as stream:
       config.read_string("[root]\n" + stream.read())
+    self.props = {}
     self.props["gInitialNumberOfRobots"] = int(config.get("root", "gInitialNumberOfRobots"))
     self.props["cMaxRobotNumber"] = int(config.get("root", "cMaxRobotNumber"))
     self.props["cMaxCattleNumber"] = int(config.get("root", "cMaxCattleNumber"))
@@ -51,11 +47,16 @@ class AgentController(Controller):
     self.controller.step()
 
 
+
 class ShepherdController:
 
   def __init__(self, agent):
     self.agent = agent
+    self.agent.instance = Pyroborobo.get()
     self.agent.set_color(*[0, 0, 255])
+    self.agent.camera_max_range = 0
+    self.agent.repulse_radius = 0
+    self.agent.orientation_radius = 0
 
   def reset(self):
     self.agent.repulse_radius = 0.2
@@ -65,10 +66,8 @@ class ShepherdController:
     self.agent.set_translation(0.25)  # Let's go forward
     self.agent.set_rotation(0)
     camera_dist = self.agent.get_all_distances()
-    camera_id = self.agent.get_all_robot_ids()
     camera_angle_rad = self.agent.get_all_sensor_angles()
     camera_angle = camera_angle_rad * 180 / np.pi
-    own_orientation = self.agent.absolute_orientation
     for i in np.argsort(camera_dist):  # get the index from the closest to the farthest
       if camera_angle[i] < -270 or camera_angle[i] > 270:
         continue
@@ -82,24 +81,28 @@ class ShepherdController:
         break  # stop
 
 
+
 class CattleController:
 
   def __init__(self, agent):
     self.agent = agent
+    self.agent.instance = Pyroborobo.get()
     self.agent.set_color(*[0, 255, 0])
+    self.agent.camera_max_range = 0
+    self.agent.repulse_radius = 0
+    self.agent.orientation_radius = 0
 
   def reset(self):
     self.agent.repulse_radius = 0.2
     self.agent.orientation_radius = 0.6
 
-  def step(self):  # step is called at each time step
+  def step(self):
     self.agent.set_translation(1)  # Let's go forward
     self.agent.set_rotation(0)
     camera_dist = self.agent.get_all_distances()
     camera_id = self.agent.get_all_robot_ids()
     camera_angle_rad = self.agent.get_all_sensor_angles()
     camera_angle = camera_angle_rad * 180 / np.pi
-    own_orientation = self.agent.absolute_orientation
     for i in np.argsort(camera_dist):  # get the index from the closest to the farthest
       if camera_angle[i] < -270 or camera_angle[i] > 270:
         continue
@@ -118,9 +121,9 @@ class CattleController:
         break  # stop
 
 
+
 if __name__ == "__main__":
-    rob = Pyroborobo.create(AgentController.config_filename,
-                            controller_class=AgentController)
-    rob.start()
-    rob.update(100000)
-    rob.close()
+  rob = Pyroborobo.create(AgentController.config_filename, controller_class=AgentController)
+  rob.start()
+  rob.update(100000)
+  rob.close()
