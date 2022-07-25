9055fa656b9354c1b89ae56b96c22fe91be9b4b9
Scott Hallauer <scott.hallauer@gmail.com>
2022-07-25 12:41:38 +0200

    Add Genome for NN + crossover and mutate functions

---
+++ pyRoborobo_dev/prj/functions/genetic.py
@@ -0,0 +1,49 @@
+import random
+from genome import Genome
+
+# reference: https://www.geeksforgeeks.org/crossover-in-genetic-algorithm/
+def crossover(method, child_id, parent_genome1, parent_genome2):
+  weights1 = parent_genome1.get_flat_weights()
+  dimensions1 = parent_genome1.get_dimensions()
+  weights2 = parent_genome2.get_flat_weights()
+  dimensions2 = parent_genome2.get_dimensions()
+  assert len(weights1) == len(weights2)
+  assert dimensions1 == dimensions2
+  n = len(weights1)
+  if method == "single":
+    cut = random.randint(1, n - 1)
+    weights3 = weights1[:cut] + weights2[cut:]
+  elif method == "double":
+    cut1 = random.randint(1, n - 2)
+    cut2 = random.randint(cut1 + 1, n - 1)
+    if random.randint(0,1) == 0:
+      weights3 = weights1[:cut1:] + weights2[cut1:cut2] + weights1[cut2:]
+    else:
+      weights3 = weights2[:cut1:] + weights1[cut1:cut2] + weights2[cut2:]
+  elif method == "uniform":
+    weights3 = []
+    for i in range(n):
+      if random.randint(0,1) == 0:
+        weights3.append(weights1[i])
+      else:
+        weights3.append(weights2[i])
+  else:
+    raise Exception("Unsupported method code for genetic crossover")
+  genome = Genome(child_id, *dimensions1)
+  genome.set_flat_weights(weights3)
+  return genome
+
+# reference: https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_mutation.htm
+def mutate(method, child_id, parent_genome, probability):
+  assert probability > 0 and probability <= 1
+  weights = parent_genome.get_flat_weights()
+  dimensions = parent_genome.get_dimensions()
+  for i in range(len(weights)):
+    if random.random() < probability:
+      if method == "bitflip":
+        weights[i] = 0 if weights[i] == 1 else 1
+      else:
+        raise Exception("Unsupported method code for genetic mutation")
+  genome = Genome(child_id, *dimensions)
+  genome.set_flat_weights(weights)
+  return genome
---
+++ pyRoborobo_dev/prj/genome.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+class Genome:
+
+  def __init__(self, id, nb_inputs, nb_hiddens, nb_outputs):
+    self.id = id
+    self.fitness_history = []
+    self.weights = None
+    self.nb_inputs = nb_inputs
+    self.nb_hiddens = nb_hiddens
+    self.nb_outputs = nb_outputs
+
+  def get_id(self):
+    return self.id
+
+  def get_dimensions(self):
+    return (self.nb_inputs, self.nb_hiddens, self.nb_outputs)
+
+  def nb_weights(self):
+    return np.sum([np.prod(layer.shape) for layer in self.weights])
+
+  def init_weights(self):
+    self.weights = [np.random.choice([0, 1], size = (self.nb_inputs, self.nb_hiddens)),
+                    np.random.choice([0, 1], size = (self.nb_hiddens, self.nb_outputs))]
+
+  def get_weights(self):
+    return self.weights
+
+  def get_flat_weights(self):
+    all_layers = []
+    for layer in self.weights:
+      all_layers.append(layer.reshape(-1))
+    flat_layers = np.concatenate(all_layers)
+    assert (flat_layers.shape == (self.nb_weights(),))
+    return flat_layers
+
+  def set_flat_weights(self, weights):
+    self.weights = []
+    self.weights.append(np.reshape(weights[:(self.nb_inputs * self.nb_hiddens):], (self.nb_inputs, self.nb_hiddens)))
+    self.weights.append(np.reshape(weights[(self.nb_inputs * self.nb_hiddens)::], (self.nb_hiddens, self.nb_outputs)))
+
+  def evaluate_network(self, inputs):
+    assert len(inputs) == self.nb_inputs
+    outputs = inputs
+    for elem in self.weights[:-1]:
+      outputs = np.tanh(outputs @ elem)
+    outputs = outputs @ self.weights[-1]  # linear output for last layer
+    return outputs # note that outputs are not constrained to a particular range (use util.clamp for range constraints)
+  
+  def add_fitness(self, score):
+    self.fitness_history.append(score)
+
+  def get_fitness(self):
+    if self.has_fitness():
+      return sum(self.fitness_history) / len(self.fitness_history)
+    else:
+      return -1
+
+  def has_fitness(self):
+    return len(self.fitness_history) != 0
