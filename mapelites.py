e882aa24254cb2dbcc12d605302363359cdc9e8f
Scott Hallauer <scott.hallauer@gmail.com>
2022-12-15 21:05:57 +0200

    Add grid initialisation and plotting to mapelites module

--- mapelites.py
+++ mapelites.py
@@ -1,12 +1,29 @@
 from qdpy.containers import Grid
+from qdpy.plots import plotGrid, plotGridSubplots
+import matplotlib.pyplot as plt
 
-def init():
+def init(_behaviour_features: list, _grid: Grid = None):
   global grid
-  maxBins = 1000
-  nbFeatures = 3
-  nbBinsPerDim = int(pow(maxBins, 1./nbFeatures))
-  nbBins = (nbBinsPerDim,) * nbFeatures
+  global behaviour_features
+  behaviour_features = _behaviour_features
+  max_bins = 1000
+  nb_features = len(behaviour_features)
+  nb_bins_per_dim = int(pow(max_bins, 1./nb_features))
+  nb_bins = (nb_bins_per_dim,) * nb_features
   max_items_per_bin = 1
   fitness_domain = ((0., 1.),)
-  features_domain = [(0., 1.)] * nbFeatures
-  grid = Grid(shape=nbBins, max_items_per_bin=max_items_per_bin, fitness_domain=fitness_domain, features_domain=features_domain, storage_type=list)
+  features_domain = [(0., 1.)] * nb_features
+  if _grid is None:
+    grid = Grid(shape=nb_bins, max_items_per_bin=max_items_per_bin, fitness_domain=fitness_domain, features_domain=features_domain, storage_type=list)
+  else:
+    grid = _grid
+
+def plot(filename: str):
+  global grid
+  global behaviour_features
+  nb_features = len(behaviour_features)
+  features_domain = [(0., 1.)] * nb_features
+  if nb_features <= 2:
+    plotGrid(grid.quality_array[...,0], filename, plt.get_cmap("nipy_spectral_r"), featuresBounds=features_domain, fitnessBounds=(0., 1.))
+  else:
+    plotGridSubplots(grid.quality_array[...,0], filename, plt.get_cmap("nipy_spectral_r"), featuresBounds=features_domain, fitnessBounds=(0., 1.))
